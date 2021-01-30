971593da8ff2d1d2ffc000fa1c7f9918460189cb
student <alidia87@gmail.com>
2021-01-30 16:55:07 +0000

    Maven Project: First Commit

---
+++ src/main/java/com/simplilearn/Lesson3/Demo/Lesson03DemoApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Lesson3.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson03DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson03DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Lesson3/Demo/Lesson03DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Lesson3.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson03DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
